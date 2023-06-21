2025ee6e547785a4ca8551675be49d0eef2af0b4
Sathishkumar R <rsathishtechit@gmail.com>
2023-06-21 11:55:51 +0530

    quantity to amoutn

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { Provider } from "react-redux";
-import { store } from "../store/store";
+import { persistor, store } from "../store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
